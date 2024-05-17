710dcbcbd6ab91e204b525140436653d3f58882b
贾星昊 <23120357@bjtu.edu.cn>
2024-05-17 22:48:00 +0800

    初始化代码库

---
+++ src/main/java/edu/bjtu/kgbridge/JenaBridgeOntologyApplication.java
@@ -0,0 +1,13 @@
+package edu.bjtu.kgbridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenaBridgeOntologyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenaBridgeOntologyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/bjtu/kgbridge/JenaBridgeOntologyApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.bjtu.kgbridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenaBridgeOntologyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
